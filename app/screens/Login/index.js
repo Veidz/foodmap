033d51c21cd0b683e1f0dd31f79cf1a7ee8c2178
Veidz <joaovictorveidz@gmail.com>
2022-01-25 00:32:01 -0300

    install base dependencies

---
+++ app/screens/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoginContainer } from './styles';
+
+const Login = () => {
+  return (
+    <LoginContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
---
+++ app/screens/Login/styles.js
@@ -0,0 +1,10 @@
+import { LinearGradient } from "expo-linear-gradient";
+import styled from "styled-components/native";
+
+export const LoginContainer = styled(LinearGradient).attrs({
+  colors: ["#380036", "#0cbaba"],
+  start: { x: 0.5, y: -0.5 },
+  end: { x: 3, y: 0.5 }
+})`
+  flex: 1;
+`
